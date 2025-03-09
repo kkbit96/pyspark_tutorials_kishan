d924a0f143c8cb388cd89396dfd0dfe14793f209
Kishan Kumar <kkishan4196@gmail.com>
2025-03-09 10:17:00 +0530

    new changes

--- coding_basics.py
+++ coding_basics.py
@@ -7,3 +7,17 @@ def move_zeros_end(arr):
             move_zeros += 1
     for i in range(move_zeros, len(arr)):
         arr[i] = 0
+
+# Remove duplicates from a sorted array
+def remove_duplicates(arr):
+    if not arr:
+        return 0
+    unique_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] != arr[unique_index]:
+            unique_index += 1
+            arr[unique_index] = arr[i]
+    return unique_index + 1
+
+def remove_duplicates_function(arr):
+    return len(set(arr))
